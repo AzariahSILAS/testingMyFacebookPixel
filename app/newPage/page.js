416c1ba74f63528cd71ffd71d5498dae6c02f529
unknown <azziesilas@gmail.com>
2025-06-21 12:43:05 -0700

    second commit

--- app/newPage/page.js
+++ app/newPage/page.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function NewPage() {
   return (
     <>
       <div className="m-[100px]">
-        <h1>this is the Home</h1>
+        <h1>this is the NewPage</h1>
         <Link
           className="mt-[10px] 
           cursor-pointer 
-          text-lg font-bold " href={"./newPage"}
+          text-lg font-bold " href={"/"}
         >
-          Click Me!
+          Take Me Home!
         </Link>
       </div>
     </>
